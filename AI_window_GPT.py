056a33826fd87f6b4fc1652b29578d50f3a36119
US986 <ilya.kulagin.06@bk.ru>
2025-11-10 19:19:46 +0300

    Add file

--- AI_window_GPT.py
+++ AI_window_GPT.py
@@ -1,8 +1,1 @@
-from AI_photo_test_GPT import analyze_photo
-
-if __name__ == '__main__':
-    test = "C:\\Users\\Старая легенда\\PycharmProjects\\Health Diary\\assets\\images\\ИИИИИ.jpg"
-
-    result = analyze_photo(test)
-    print("Результат анализа")
-    print(result)
+#Код будет загружен в Git позже, так как он содержит конфиденциальную информацию.
